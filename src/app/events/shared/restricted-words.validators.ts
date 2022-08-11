dc72b57407c69538492d8e2693b0a17b09e230c3
Benito Ishimwe <benishimwe31@gmail.com>
2022-08-11 20:23:21 +0200

    Added Restricted words

--- src/app/events/shared/restricted-words.validators.ts
+++ src/app/events/shared/restricted-words.validators.ts
@@ -1,30 +1,32 @@
-// import { FormControl } from "@angular/forms"
+import { FormControl } from "@angular/forms"
 
-// export function restrictedWords(words:any){
-//   return (control: FormControl): {[key:string]:any} => {
-//    if (!words) return null
+export function restrictedWords(words:any){
+  return (control: FormControl): {[key:string]:any} => {
+   if (!words) return null
    
-//    var invalidWords = words
-//     .map((w:any) => control.value.includes(w) ? w : null)
-//     .filter((w:any)  => w != null)
+   var invalidWords = words
+    .map((w:any) => control.value.includes(w) ? w : null)
+    .filter((w:any)  => w != null)
 
-//     return (invalidWords:any) => invalidWords.length > 0
-//     ? {'restrictedWords': invalidWords.join(', ')}
-//     : null 
-//   }
+    return (invalidWords:any) => invalidWords.length > 0
+    ? {'restrictedWords': invalidWords.join(', ')}
+    : null 
+  }
+}
 
 
   // export function restrictedWords(words){
-  // return (control: FormControl): {[key:string]:any} => {
-  //  if (!words) return null
-   
-  //  var invalidWords = words
-  //   .map(w => control.value.includes(w) ? w : null)
-  //   .filter(w => w != null)
+  //   return (control: FormControl): {[key:string]:any} => {
+  //   if (!words) return null
+    
+  //   var invalidWords = words
+  //     .map(w => control.value.includes(w) ? w : null)
+  //     .filter(w => w != null)
 
-  //   return invalidWords && invalidWords.length > 0
-  //   ? {'restrictedWords': invalidWords.join(', ')}
-  //   : null 
+  //     return invalidWords && invalidWords.length > 0
+  //     ? {'restrictedWords': invalidWords.join(', ')}
+  //     : null 
+  //   }
   // }
  
 
@@ -33,4 +35,4 @@
   //  :null
   
   
-//  }
+ //  }
